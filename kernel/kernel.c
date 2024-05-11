e12921800402719516f61a7bb046541159f82e13
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-05-11 23:41:23 +0900

    why hello world is not printed out?

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,8 @@
 #include "mbox.h"
 #include "../uart/uart0.h"
 #include "../uart/uart1.h"
-#include "framebf.h"
+#include "../lib/framebf.h"
+#include "../lib/utils.h"
 
 void main()
 {
@@ -9,15 +10,18 @@ void main()
     uart_init();
     // say hello
     uart_puts("\n\nHello World!\n");
-    // Initialize frame buffer
-    framebf_init();
-    // Draw something on the screen
-    drawRectARGB32(100, 100, 400, 400, 0x00AA0000, 1); // RED
-    drawRectARGB32(150, 150, 400, 400, 0x0000BB00, 1); // GREEN
-    drawRectARGB32(200, 200, 400, 400, 0x000000CC, 1); // BLUE
-    drawRectARGB32(250, 250, 400, 400, 0x00FFFF00, 1); // YELLOW
-    drawPixelARGB32(300, 300, 0x00FF0000);             // RED
+    uart_puts("\n\nHello World!\n");
+    uart_puts("\n\nHello World!\n");
 
+    // Initialize frame buffer
+    // framebf_init();
+    // // Draw something on the screen
+    // drawRectARGB32(100, 100, 400, 400, 0x00AA0000, 1); // RED
+    // drawRectARGB32(150, 150, 400, 400, 0x0000BB00, 1); // GREEN
+    // drawRectARGB32(200, 200, 400, 400, 0x000000CC, 1); // BLUE
+    // drawRectARGB32(250, 250, 400, 400, 0x00FFFF00, 1); // YELLOW
+    // drawPixelARGB32(300, 300, 0x00FF0000);             // RED
+    drawString(100, 400, "Hello world", 0x0000BB00, 1);
     while (1)
     {
         // read each char
---
+++ lib/utils.h
@@ -0,0 +1,5 @@
+void drawChar(unsigned char ch, int x, int y, unsigned int attr, int zoom);
+void drawString(int x, int y, char *str, unsigned int attr, int zoom);
+unsigned char getUart();
+void wait_msec(unsigned int msVal);
+void set_wait_timer(int set, unsigned int msVal);
