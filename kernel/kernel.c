8dcb3f6c6c2860cf0a382abd59e21cb523dceccb
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-05-12 22:26:00 +0900

    show string complete

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -14,7 +14,7 @@ void main()
     uart_puts("\n\nHello World!\n");
 
     // Initialize frame buffer
-    // framebf_init();
+    framebf_init();
     // // Draw something on the screen
     // drawRectARGB32(100, 100, 400, 400, 0x00AA0000, 1); // RED
     // drawRectARGB32(150, 150, 400, 400, 0x0000BB00, 1); // GREEN
@@ -22,8 +22,8 @@ void main()
     // drawRectARGB32(250, 250, 400, 400, 0x00FFFF00, 1); // YELLOW
     // drawPixelARGB32(300, 300, 0x00FF0000);             // RED
 
-    drawChar("A", 200, 400, 0x00AA0000, 3);
-    // drawString(200, 400, "Hello world", 0x0000BB00, 100);
+    // drawChar("A", 200, 400, 0x00AA0000, 3);
+    drawString(200, 400, "Hello world", 0x0000BB00, 1);
     while (1)
     {
         // read each char
