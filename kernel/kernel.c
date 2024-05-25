03d64a1a7b0d0be425fa083c4b6f382aa94127f2
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2024-05-26 02:21:49 +0700

    multicore testing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,6 +4,7 @@
 #include "../lib/framebf.h"
 #include "../lib/utils.h"
 #include "../lib/game.h"
+#include "../lib/multicore.h"
 
 // int w_index = [ 24, 99, 174, 249, 324, 399, 474, 549, 624, 699, 774, 849, 924 ];
 // int h_index = [ 0, 57, 114, 171, 228, 285, 342, 399, 456, 513, 570, 627, 684 ];
@@ -31,122 +32,170 @@ int direction; // 1 is right, 0 is left
 void game_start_fn();
 int is_die_check(int current_character, int current_block, int timer);
 
-void main()
-{
+// void main()
+// {
+//     // set up serial console
+//     uart_init();
+
+    // Initialize frame buffer
+    // framebf_init();
+
+    // while (1)
+    // {
+    //     if (game_start == 1)
+    //     {
+
+    //         if (stage == 1)
+    //         {
+
+    //             if (step == 0)
+    //             {
+    //                 showBackground(shiftY, stage);
+    //                 create_block(block_array);
+    //             }
+    //             if (gmae_over_flag == 0)
+    //             {
+    //                 load_character(current_w_index, current_h_index, direction);
+    //             }
+    //         }
+
+    //         else if (stage == 2)
+    //         {
+    //             // shiftY = shiftY + 50;
+    //             showBackground(shiftY, stage);
+    //         }
+    //         else if (stage == 3)
+    //         {
+    //             // shiftY = shiftY + 50;
+    //             showBackground(shiftY, stage);
+    //         }
+
+    //         wait_msec(10);
+    //         char c = uart_getc();
+
+    //         if (c == 'a')
+    //         {
+    //             step += 1;
+    //             current_w_index -= 75;
+    //             current_h_index -= 57;
+    //             direction = 0;
+    //         }
+
+    //         if (c == 'd')
+    //         {
+    //             step += 1;
+    //             current_w_index += 75;
+    //             current_h_index -= 57;
+    //             direction = 1;
+    //         }
+    //         if (step == 12)
+    //         {
+    //             step = 0;
+    //             shiftY += 100;
+    //             current_h_index = 708 - 120;
+    //             block_array = create_block_array(block_array[12]);
+    //         }
+
+    //         // die logic
+    //         if (step != 0)
+    //         {
+    //             gmae_over_flag = is_die_check(current_w_index, block_array[step], stage_1_timer);
+    //         }
+
+    //         if (gmae_over_flag == 0)
+    //         {
+    //             ms_counter++;
+    //             if (ms_counter == 3)
+    //             {
+    //                 // check time about 1 second
+    //                 ms_counter = 0;
+    //                 stage_1_timer -= 1;
+    //             }
+    //             show_timer(stage_1_timer);
+    //             // move logic
+
+    //             if (shiftY > 450)
+    //             {
+    //                 shiftY = -350;
+    //                 stage++;
+    //             }
+    //         }
+
+    //         if (gmae_over_flag == 1) // game over
+    //         {
+    //             current_h_index += 100;
+    //             if (direction == 1)
+    //             {
+    //                 current_w_index += 50;
+    //             }
+    //             else if (direction == 0)
+    //             {
+    //                 current_w_index -= 50;
+    //             }
+    //             show_die_character_fn(current_w_index, current_h_index, direction);
+    //             wait_msec(500);
+    //             show_game_over_fn();
+
+    //             gmae_over_flag = 0;
+    //             game_start = 0;
+    //         }
+    //     }
+    //     else if (game_start == 0)
+    //     {
+    //         game_init_fn();
+    //         game_start = 1;
+    //         game_start_fn();
+    //         block_array = create_block_array(first_block);
+    //     }
+    // }
+// }
+
+void core3_main(void){
+    clear_core3();
+    for (int i = 0; i < 10; i++){
+        uart_puts("Core 3 is running\n");
+        wait_msec(500);
+    }
+}
+
+void core2_main(void){
+    clear_core2();
+    uart_puts("Core 2 is running\n");
+    for (int i = 0; i < 10; i++){
+        uart_puts("Core 2 is running\n");
+        wait_msec(2000);
+    }
+}
+
+void core1_main(void){
+    clear_core1();
+    uart_puts("Core 1 is running\n");
+    for (int i = 0; i < 10; i++){
+        uart_puts("Core 1 is running\n");
+        wait_msec(1000);
+    }
+}
+
+void core0_main(void){
+    uart_puts("Core 0 is running\n");
+    for (int i = 0; i < 10; i++){
+        uart_puts("Core 0 is running\n");
+        wait_msec(500);
+    }
+}
+
+void main(){
     // set up serial console
     uart_init();
 
     // Initialize frame buffer
     framebf_init();
 
-    while (1)
-    {
-        if (game_start == 1)
-        {
-
-            if (stage == 1)
-            {
-
-                if (step == 0)
-                {
-                    showBackground(shiftY, stage);
-                    create_block(block_array);
-                }
-                if (gmae_over_flag == 0)
-                {
-                    load_character(current_w_index, current_h_index, direction);
-                }
-            }
-
-            else if (stage == 2)
-            {
-                // shiftY = shiftY + 50;
-                showBackground(shiftY, stage);
-            }
-            else if (stage == 3)
-            {
-                // shiftY = shiftY + 50;
-                showBackground(shiftY, stage);
-            }
-
-            wait_msec(10);
-            char c = uart_getc();
-
-            if (c == 'a')
-            {
-                step += 1;
-                current_w_index -= 75;
-                current_h_index -= 57;
-                direction = 0;
-            }
-
-            if (c == 'd')
-            {
-                step += 1;
-                current_w_index += 75;
-                current_h_index -= 57;
-                direction = 1;
-            }
-            if (step == 12)
-            {
-                step = 0;
-                shiftY += 100;
-                current_h_index = 708 - 120;
-                block_array = create_block_array(block_array[12]);
-            }
-
-            // die logic
-            if (step != 0)
-            {
-                gmae_over_flag = is_die_check(current_w_index, block_array[step], stage_1_timer);
-            }
-
-            if (gmae_over_flag == 0)
-            {
-                ms_counter++;
-                if (ms_counter == 3)
-                {
-                    // check time about 1 second
-                    ms_counter = 0;
-                    stage_1_timer -= 1;
-                }
-                show_timer(stage_1_timer);
-                // move logic
-
-                if (shiftY > 450)
-                {
-                    shiftY = -350;
-                    stage++;
-                }
-            }
-
-            if (gmae_over_flag == 1) // game over
-            {
-                current_h_index += 100;
-                if (direction == 1)
-                {
-                    current_w_index += 50;
-                }
-                else if (direction == 0)
-                {
-                    current_w_index -= 50;
-                }
-                show_die_character_fn(current_w_index, current_h_index, direction);
-                wait_msec(500);
-                show_game_over_fn();
-
-                gmae_over_flag = 0;
-                game_start = 0;
-            }
-        }
-        else if (game_start == 0)
-        {
-            game_init_fn();
-            game_start = 1;
-            game_start_fn();
-            block_array = create_block_array(first_block);
-        }
-    }
+    // Initialize multicore
+    // start_core1(core1_main);
+    // start_core2(core2_main);
+    start_core3(core3_main);
+    // core0_main();
 }
 
 int is_die_check(int current_character, int current_block, int timer)
---
+++ lib/multicore.c
@@ -0,0 +1,53 @@
+#include "multicore.h"
+// extern unsigned long spin_cpu0;
+// extern unsigned long spin_cpu1;
+// extern unsigned long spin_cpu2;
+// extern unsigned long spin_cpu3;
+
+unsigned long spin_cpu0;
+unsigned long spin_cpu1;
+unsigned long spin_cpu2;
+unsigned long spin_cpu3;
+
+void store32(unsigned long address, unsigned long value)
+{
+    *(unsigned long *) address = value;
+}
+
+unsigned long load32(unsigned long address)
+{
+    return *(unsigned long *) address;
+}
+
+void start_core1(void (*func)(void))
+{
+    store32((unsigned long)&spin_cpu1, (unsigned long)func);
+    asm volatile ("sev");
+}
+
+void start_core2(void (*func)(void))
+{
+    store32((unsigned long)&spin_cpu2, (unsigned long)func);
+    asm volatile ("sev");
+}
+
+void start_core3(void (*func)(void))
+{
+    store32((unsigned long)&spin_cpu3, (unsigned long)func);
+    asm volatile ("sev");
+}
+
+void clear_core1(void) 
+{
+    store32((unsigned long)&spin_cpu1, 0);
+}
+
+void clear_core2(void) 
+{
+    store32((unsigned long)&spin_cpu2, 0);
+}
+
+void clear_core3(void) 
+{
+    store32((unsigned long)&spin_cpu3, 0);
+}
